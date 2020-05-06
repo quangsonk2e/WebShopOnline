d83c5d1b1568c025ff3bd8efac3058f0c4b5ff63
quang son <quangsonk2e@gmail.com>
2020-05-06 21:59:44 +0700

    test

---
+++ Web_OnlineShop/Web_OnlineShop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_OnlineShop.DAO_OnlineShop;
+using PagedList;
+using PagedList.Mvc;
+
+namespace Web_OnlineShop.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /Admin/User/
+        public ActionResult Index(int page=1)
+        {
+            var Model = new UserDao().getUserPage(page);
+            return View(Model);
+        }
+	}
+}
