7364bdbaa4da360a0f42c43825a241f05d492b9a
quang son <quangsonk2e@gmail.com>
2020-05-11 09:37:42 +0700

    Template

--- Web_OnlineShop/Web_OnlineShop/App_Start/RouteConfig.cs
+++ Web_OnlineShop/Web_OnlineShop/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Web_OnlineShop
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Web_OnlineShop.Controllers" }
             );
         }
     }
--- Web_OnlineShop/Web_OnlineShop/Areas/Admin/AdminAreaRegistration.cs
+++ Web_OnlineShop/Web_OnlineShop/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace Web_OnlineShop.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "Web_OnlineShop.Areas.Admin.Controllers" }
             );
         }
     }
