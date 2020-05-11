fd141ce1bde598cd0c890cac7d75f2b8206427ba
quang son <quangsonk2e@gmail.com>
2020-05-11 16:05:54 +0700

    11/5

---
+++ Web_OnlineShop/Web_OnlineShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_OnlineShop.DAO_OnlineShop;
+using Web_OnlineShop.ModelOnlineShop;
+
+namespace Web_OnlineShop.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Admin/Product/
+        public ActionResult Index(int page=1)
+        {
+            var products = new ProductDao().getProductPage(page);
+            return View(products);
+        }
+        [HttpGet]
+        public ActionResult Create(){
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+
+            return View();
+        }
+	}
+}
---
+++ Web_OnlineShop/Web_OnlineShop/DAO_OnlineShop/ProductDao.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web_OnlineShop.ModelOnlineShop;
+using PagedList;
+namespace Web_OnlineShop.DAO_OnlineShop
+{
+    public class ProductDao
+    {
+        Shop db = null;
+        public ProductDao()
+        {
+            db = new Shop();
+        }
+        public List<Product> getAll()
+        {
+            return db.Products.ToList();
+        }
+        public int CountAll()
+        {
+            return db.Products.Count();
+        }
+
+        public IEnumerable<Product> getProductPage(int pageNumber = 1)
+        {
+            return db.Products.OrderBy(x => x.CreatedDate).ToPagedList(pageNumber, DEFINE.pageSize);
+        }
+        public Product getById(long id)
+        {
+            return db.Products.SingleOrDefault(x => x.ID == id);
+        }
+        public long insert(Product Product)
+        {
+            db.Products.Add(Product);
+            db.SaveChanges();
+            return Product.ID;
+        }
+        public long update(Product Product)
+        {
+            var us = db.Products.Find(Product.ID);
+            us.Name = Product.Name;
+            //us.ProductName = Product.ProductName;
+            //us.ModifieldDate = DateTime.Now;
+            db.SaveChanges();
+            return 1;
+        }
+        public long delete(long id)
+        {
+
+            db.Products.Remove(db.Products.Find(id));
+            db.SaveChanges();
+            return 1;
+        }
+    }
+}
